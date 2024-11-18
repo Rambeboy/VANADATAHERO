24514722aee6b75d82a3245ca75c3d2e9e637ecc
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-18 13:52:44 +0700

    Update twist.js

--- app/src/utils/twist.js
+++ app/src/utils/twist.js
@@ -1,1 +1,43 @@
-function _0x48ef(){const _0x50467d=['58020UvwSiT','780oNPlWy','1862475zgMTMi','158878RsnnAl','tasks','\x0a==============================================\x0aInfo\x20:\x20','completed','filter','length','flush','clear','2989728xmTxXC','1898596cadvdX','33848727yVUZFb','\x20=============\x0a','954fcCUEI','spinnerContent','19860BjHzAL','remove','\x0a=================\x20Account\x20','twisters','points','info','8nttGMx','1awSWkU','put'];_0x48ef=function(){return _0x50467d;};return _0x48ef();}const _0xf0b83=_0x1165;(function(_0x59e782,_0x35afdf){const _0xb5e3f0=_0x1165,_0xf7219c=_0x59e782();while(!![]){try{const _0x29671f=parseInt(_0xb5e3f0(0x1c7))/0x1*(-parseInt(_0xb5e3f0(0x1cc))/0x2)+-parseInt(_0xb5e3f0(0x1cb))/0x3+-parseInt(_0xb5e3f0(0x1d5))/0x4+parseInt(_0xb5e3f0(0x1c0))/0x5*(-parseInt(_0xb5e3f0(0x1ca))/0x6)+parseInt(_0xb5e3f0(0x1d4))/0x7*(-parseInt(_0xb5e3f0(0x1c6))/0x8)+parseInt(_0xb5e3f0(0x1be))/0x9*(-parseInt(_0xb5e3f0(0x1c9))/0xa)+parseInt(_0xb5e3f0(0x1d6))/0xb;if(_0x29671f===_0x35afdf)break;else _0xf7219c['push'](_0xf7219c['shift']());}catch(_0x982bf3){_0xf7219c['push'](_0xf7219c['shift']());}}}(_0x48ef,0x53ed8));import{Twisters}from'twisters';function _0x1165(_0x37b294,_0x13916a){const _0x48ef78=_0x48ef();return _0x1165=function(_0x1165da,_0x514973){_0x1165da=_0x1165da-0x1bd;let _0x4a6007=_0x48ef78[_0x1165da];return _0x4a6007;},_0x1165(_0x37b294,_0x13916a);}import _0x3e9198 from'./logger.js';import{Core}from'../core/core.js';import{Helper}from'./helper.js';export class Twist{constructor(){const _0x592be7=_0x1165;this[_0x592be7(0x1c3)]=new Twisters();}['log'](_0x1c1f9d='',_0x5cf96e='',_0x397f8f=new Core(),_0x31cf11){const _0x2a4676=_0x1165;_0x31cf11==undefined&&(_0x3e9198[_0x2a4676(0x1c5)](_0x5cf96e['id']+'\x20-\x20'+_0x1c1f9d),_0x31cf11='-');const _0x378897=_0x397f8f['user']??{},_0x206aa1=_0x378897[_0x2a4676(0x1c4)]??'-',_0x452c50=_0x397f8f[_0x2a4676(0x1cd)]??[],_0x5175c3=_0x452c50[_0x2a4676(0x1d1)]??'-',_0x1f9277=_0x452c50[_0x2a4676(0x1d0)](_0xedbd1b=>_0xedbd1b[_0x2a4676(0x1cf)]['length']!=0x0)[_0x2a4676(0x1d1)]??'-';let _0x3902a0={'msg':_0x1c1f9d,'delay':_0x31cf11,'acc':_0x5cf96e,'points':_0x206aa1,'availableTask':_0x5175c3,'completedTask':_0x1f9277};this[_0x2a4676(0x1c3)][_0x2a4676(0x1c8)](_0x5cf96e['id'],{'text':_0x2a4676(0x1c2)+_0x5cf96e['id']+_0x2a4676(0x1bd)+Helper[_0x2a4676(0x1bf)](_0x3902a0)+'\x0a=============================================='});}[_0xf0b83(0x1c5)](_0xeabf5e=''){const _0x50bfc6=_0xf0b83;this[_0x50bfc6(0x1c3)][_0x50bfc6(0x1c8)](0x2,{'text':_0x50bfc6(0x1ce)+_0xeabf5e+'\x0a=============================================='});return;}['clearInfo'](){const _0x1fafce=_0xf0b83;this[_0x1fafce(0x1c3)][_0x1fafce(0x1c1)](0x2);}async[_0xf0b83(0x1d3)](_0x2d720b){const _0x4978de=_0xf0b83;await this['twisters'][_0x4978de(0x1d2)]();}}
+import { Twisters } from 'twisters';
+import _0x3e9198 from './logger.js';
+import { Core } from '../core/core.js';
+import { Helper } from './helper.js';
+export class Twist {
+  constructor() {
+    this.twisters = new Twisters();
+  }
+  ['log'](_0x1c1f9d = '', _0x5cf96e = '', _0x397f8f = new Core(), _0x31cf11) {
+    if (_0x31cf11 == undefined) {
+      _0x3e9198.info(_0x5cf96e.id + " - " + _0x1c1f9d);
+      _0x31cf11 = '-';
+    }
+    const _0x378897 = _0x397f8f.user ?? {};
+    const _0x206aa1 = _0x378897.points ?? '-';
+    const _0x452c50 = _0x397f8f.tasks ?? [];
+    const _0x5175c3 = _0x452c50.length ?? '-';
+    const _0x1f9277 = _0x452c50.filter(_0xedbd1b => _0xedbd1b.completed.length != 0x0).length ?? '-';
+    let _0x3902a0 = {
+      'msg': _0x1c1f9d,
+      'delay': _0x31cf11,
+      'acc': _0x5cf96e,
+      'points': _0x206aa1,
+      'availableTask': _0x5175c3,
+      'completedTask': _0x1f9277
+    };
+    this.twisters.put(_0x5cf96e.id, {
+      'text': "\n================= Account " + _0x5cf96e.id + " =============\n" + Helper.spinnerContent(_0x3902a0) + "\n=============================================="
+    });
+  }
+  ["info"](_0xeabf5e = '') {
+    this.twisters.put(0x2, {
+      'text': "\n==================================================\nInfo : " + _0xeabf5e + "\n=============================================="
+    });
+    return;
+  }
+  ['clearInfo']() {
+    this.twisters.remove(0x2);
+  }
+  async ["clear"](_0x2d720b) {
+    await this.twisters.flush();
+  }
+}
